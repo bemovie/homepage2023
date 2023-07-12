47500484b4dc92e044b01e93f95cd23ce949aa5f
Student <Student@Student-PC>
2023-07-12 21:12:39 +0900

    2023-07-12

---
+++ homepage2023/src/main/java/egovframework/let/join/web/JoinController.java
@@ -0,0 +1,122 @@
+package egovframework.let.join.web;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.service.EgovFileMngService;
+import egovframework.com.cmm.service.FileVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.let.board.service.BoardService;
+import egovframework.let.board.service.BoardVO;
+import egovframework.let.crud.service.CrudVO;
+import egovframework.let.join.service.JoinService;
+import egovframework.let.join.service.JoinVO;
+import egovframework.let.utl.fcc.service.EgovStringUtil;
+import egovframework.let.utl.fcc.service.FileMngUtil;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+import net.sf.json.JSONObject;
+
+
+@Controller
+public class JoinController {
+	
+	@Resource(name = "joinService")
+	private JoinService joinService;
+	
+	@Resource(name = "egovMessageSource")
+	EgovMessageSource egovMessageSource;
+	
+	//약관동의
+	@RequestMapping(value = "/join/siteUseAgree.do")
+	public String siteUseAgree(@ModelAttribute("searchVO") JoinVO vo, HttpServletRequest request, ModelMap model, HttpSession session) throws Exception {
+		
+		return "join/SiteUseAgree";
+	}
+	
+	//회원구분
+	@RequestMapping(value = "/join/memberType.do")
+	public String memberType(@ModelAttribute("searchVO") JoinVO vo, HttpServletRequest request, ModelMap model, HttpSession session) throws Exception {
+		
+		return "join/MemberType";
+	}
+	
+	//회원등록 폼
+	@RequestMapping(value = "/join/memberRegist.do")
+	public String memberRegist(@ModelAttribute("searchVO") JoinVO vo, HttpServletRequest request, ModelMap model/*, HttpSession session*/) throws Exception {
+		
+		return "join/MemberRegist";
+	}
+	
+	//아이디 중복체크
+	@RequestMapping(value= "/join/duplicateCheck.do")
+	public void duplicateCheck(@ModelAttribute("searchVO") JoinVO vo, HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception {
+		String successYn = "Y";
+		String message = "성공";
+		
+		JSONObject jo = new JSONObject();
+		response.setContentType("application/json; charset=utf-8"); 
+		
+		int duplicateCnt = joinService.duplicateCheck(vo);
+		if(duplicateCnt > 0) {
+			successYn = "N";
+			message = egovMessageSource.getMessage("fail.duplicate.member"); //이미 사용중인 ID입니다.;
+		}
+		
+//		List<Map> mapList = new ArrayList();
+//		Map test = new HashMap();
+//		test.put("key", "1");
+//		test.put("name", "성진1");
+//		mapList.add(test);
+//		mapList.add(test);
+		
+//		EgovMap map2 = new EgovMap();
+//		map2.put("totalCount", 10);
+//		map2.put("mapList", mapList);
+		
+		jo.put("successYn", successYn);
+		jo.put("message", message);
+//		jo.put("data", mapList);
+//		jo.put("data", map2);	
+		
+		PrintWriter printwriter = response.getWriter();
+		printwriter.println(jo.toString());
+		printwriter.flush();
+		printwriter.close();
+	}
+	
+	//회원가입
+	@RequestMapping(value="/join/insertMember.do")
+	public String insertMember(@ModelAttribute("searchVO") JoinVO vo, HttpServletRequest request, ModelMap model) throws Exception{
+		if(joinService.duplicateCheck(vo) > 0) {
+			model.addAttribute("message", egovMessageSource.getMessage("fail.duplicate.member")); //이미 사용중인 ID입니다.
+			return "forward:/join/memberType.do";
+		} else {
+			joinService.insertJoin(vo);
+			model.addAttribute("message", egovMessageSource.getMessage("join.request.msg")); //회원신청이 정상적으로 완료되었습니다. \n로그인 후 이용해 주세요.
+		}
+		
+		return "join/MemberComplete";
+	}
+	
+	
+	
+}
--- homepage2023/src/main/webapp/WEB-INF/jsp/board/BoardSelect.jsp
+++ homepage2023/src/main/webapp/WEB-INF/jsp/board/BoardSelect.jsp
@@ -9,7 +9,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Language" content="ko" >
-<meta http-equiv="X-UA-Compatible" content="IE=dege" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
 <title>수업용 게시판</title>
 <script src="https://code.jquery.com/jquery-latest.min.js"></script>
